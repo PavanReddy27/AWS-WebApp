34473b946a777ce30b9e53756f305e8a9d5356b7
PavanReddy27 <148244818+PavanReddy27@users.noreply.github.com>
2023-10-17 23:28:12 +0530

    Update1 config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ap-south-1_BQzt3bLkN', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6gcmjmql2s77sdkhna9tehvtep', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-south-1' // e.g. us-east-2
+        region: 'ap-south-1' // e.g. us-east
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
